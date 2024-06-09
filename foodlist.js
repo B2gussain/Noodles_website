6a23b32a73c5b89811bbdc9e2168a36956c93c74
Bittu <153991226+B2gussain@users.noreply.github.com>
2024-06-09 13:15:44 +0530

    Add files via upload

---
+++ foodlist.js
@@ -0,0 +1,33 @@
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000, // 3000 milliseconds = 3 seconds
+    disableOnInteraction: false, // Autoplay will not be disabled after interactions
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
